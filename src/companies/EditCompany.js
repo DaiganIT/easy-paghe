779f515fb8da9db2b79e97752508386a1ee2d048
Pietro Carta <pietro.carta88@gmail.com>
2018-11-08 20:22:06 +0000

    add company form

--- src/companies/EditCompany.js
+++ src/companies/EditCompany.js
@@ -1,9 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import classnames from 'classnames';
 import { withStyles } from '@material-ui/core';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 import http from './http';
 
 const styles = {
@@ -15,11 +22,18 @@ const styles = {
 		padding: '1em',
 		marginBottom: '1em',
 	},
+	companyDetails: {
+		marginRight: '1em',
+	},
 	grow: {
 		flexGrow: 1,
 	},
 	textField: {
 		marginRight: '1em',
+		marginBottom: '1em',
+	},
+	miniTitle: {
+		marginBottom: '1em',
 	},
 };
 function EditCompany({ classes }) {
@@ -27,6 +41,7 @@ function EditCompany({ classes }) {
 	const [name, setName] = useState('');
 	const [address, setAddress] = useState('');
 	const [phone, setPhone] = useState('');
+	const [employees, setEmployees] = useState([]);
 
 	useEffect(
 		() => {
@@ -54,13 +69,73 @@ function EditCompany({ classes }) {
 					Aggiungi Azienda
 				</Typography>
 			</span>
-			<Paper className={classes.paper}>
-				<form>
-					<TextField label="Nome" className={classes.textField} disabled={isSaving} value={name} onChange={e => setName(e.target.value)} />
-					<TextField label="Indirizzo" className={classes.textField} disabled={isSaving} value={address} onChange={e => setAddress(e.target.value)} />
-					<TextField label="Telefono" className={classes.textField} disabled={isSaving} value={phone} onChange={e => setPhone(e.target.value)} />
-				</form>
-			</Paper>
+			<form>
+				<Grid container>
+					<Grid item xs={6}>
+						<Paper className={classnames(classes.paper, classes.companyDetails)}>
+							<Typography variant="title" className={classes.miniTitle}>
+								Dettagli azienda
+							</Typography>
+							<TextField
+								variant="outlined"
+								label="Nome"
+								fullWidth
+								className={classes.textField}
+								disabled={isSaving}
+								value={name}
+								onChange={(e) => setName(e.target.value)}
+							/>
+							<TextField
+								variant="outlined"
+								label="Indirizzo"
+								fullWidth
+								className={classes.textField}
+								disabled={isSaving}
+								value={address}
+								onChange={(e) => setAddress(e.target.value)}
+							/>
+							<TextField
+								variant="outlined"
+								label="Telefono"
+								fullWidth
+								className={classes.textField}
+								disabled={isSaving}
+								value={phone}
+								onChange={(e) => setPhone(e.target.value)}
+							/>
+						</Paper>
+					</Grid>
+					<Grid item xs={6}>
+						<Paper className={classes.paper}>
+							<Typography variant="title" className={classes.miniTitle}>
+								Dipendenti
+							</Typography>
+							<Table>
+								<TableHead>
+									<TableRow>
+										<TableCell>Nome</TableCell>
+										<TableCell>Indirizzo</TableCell>
+										<TableCell>Telefono</TableCell>
+										<TableCell>Email</TableCell>
+									</TableRow>
+								</TableHead>
+								<TableBody>
+									{employees.map((employee) => (
+										<TableRow key={employee.id}>
+											<TableCell component="th" scope="row">
+												{employee.name}
+											</TableCell>
+											<TableCell>{employee.address}</TableCell>
+											<TableCell>{employee.phone}</TableCell>
+											<TableCell>{employee.email}</TableCell>
+										</TableRow>
+									))}
+								</TableBody>
+							</Table>
+						</Paper>
+					</Grid>
+				</Grid>
+			</form>
 			<Button variant="contained" size="small" color="primary" onClick={save} disabled={isSaving}>
 				Salva
 			</Button>
