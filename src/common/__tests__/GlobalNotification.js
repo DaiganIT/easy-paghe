d6a658aafe731e98b9038b03a231e72646aa1608
Pietro Carta <pietro.carta88@gmail.com>
2019-04-10 14:31:18 +0100

    added tests for GlobalNotification

---
+++ src/common/__tests__/GlobalNotification.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {
+  render,
+  fireEvent,
+  cleanup,
+  waitForElement,
+  act,
+} from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import GlobalNotification from '../GlobalNotification';
+import EventBus from 'eventbusjs';
+
+afterEach(cleanup)
+
+describe('GIVEN I have a Global Notification', () => {
+  test('THEN The component is not rendered', () => {
+    const { container } = render(<GlobalNotification>
+      Save
+    </GlobalNotification>)
+
+    expect(container.outerHTML).toBe('<div></div>');
+  });
+  describe('WHEN A notification event is emitted', () => {
+    test('THEN The component is rendered', () => {
+      const { container } = render(<GlobalNotification>
+        Save
+      </GlobalNotification>);
+      
+      act(() => {
+        EventBus.dispatch('global-notification-show', undefined, { message: 'Azienda creata' });
+      });
+
+      expect(container.querySelector('.MuiSnackbar-root-10')).toBeTruthy();
+      expect(container.textContent).toBe('Azienda creata');
+    });
+  });
+});
