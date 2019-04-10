87ce41ca00990ae3f32182e40bd5fef63f240469
Pietro Carta <pietro.carta88@gmail.com>
2019-04-10 11:44:12 +0100

    using jest mock utils for promises

--- src/auth/__tests__/auth.js
+++ src/auth/__tests__/auth.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import axios from 'axios';
+import axiosMock from 'axios';
 import authentication from '../auth';
 
 describe('GIVEN the local storage has no user', () => {
@@ -16,22 +16,20 @@ describe('GIVEN the local storage has no user', () => {
 
 describe('GIVEN I have correct username and password', () => {
   beforeAll(() => {
-    authentication.isAuthenticated = null;
     jest.clearAllMocks();
-    axios.post = jest.fn(() => new Promise((resolve, reject) => {
-      resolve({ name: 'Pietro' });
-    }));
+    authentication.isAuthenticated = null;
   });
 
   describe('WHEN I login', () => {
-    beforeAll(async () => {
-      await authentication.login('username', 'password');
+    beforeAll(() => {
+      axiosMock.post.mockResolvedValueOnce({ name: 'Pietro' });
+      authentication.login('username', 'password');
     });
 
     test('THEN The login url is called with the correct data', () => {
-      expect(axios.post.mock.calls.length).to.equal(1);
-      expect(axios.post.mock.calls[0][0]).to.equal('/api/auth/login');
-      expect(axios.post.mock.calls[0][1]).to.deep.equal({ username: 'username', password: 'password' });
+      expect(axiosMock.post.mock.calls.length).to.equal(1);
+      expect(axiosMock.post.mock.calls[0][0]).to.equal('/api/auth/login');
+      expect(axiosMock.post.mock.calls[0][1]).to.deep.equal({ username: 'username', password: 'password' });
     });
 
     test('THEN The user is authenticated', () => {
@@ -44,21 +42,19 @@ describe('GIVEN I have wrong username and password', () => {
   beforeAll(() => {
     jest.clearAllMocks();
     authentication.isAuthenticated = null;
-    axios.post = jest.fn(() => new Promise((resolve, reject) => {
-      reject();
-    }));
   });
 
   describe('WHEN I login', () => {
-    beforeAll(async () => {
-      await authentication.login('username', 'password')
-        .catch(() => {});
+    beforeAll(() => {
+      axiosMock.post.mockRejectedValueOnce();
+      authentication.login('username', 'password')
+        .catch(() => { });
     });
 
     test('THEN The login url is called with the correct data', () => {
-      expect(axios.post.mock.calls.length).to.equal(1);
-      expect(axios.post.mock.calls[0][0]).to.equal('/api/auth/login');
-      expect(axios.post.mock.calls[0][1]).to.deep.equal({ username: 'username', password: 'password' });
+      expect(axiosMock.post.mock.calls.length).to.equal(1);
+      expect(axiosMock.post.mock.calls[0][0]).to.equal('/api/auth/login');
+      expect(axiosMock.post.mock.calls[0][1]).to.deep.equal({ username: 'username', password: 'password' });
     });
 
     test('THEN The user is not authenticated', () => {
@@ -69,22 +65,20 @@ describe('GIVEN I have wrong username and password', () => {
 
 describe('GIVEN I\'m logged in', () => {
   beforeAll(() => {
-    authentication.isAuthenticated = null;
     jest.clearAllMocks();
+    authentication.isAuthenticated = null;
     localStorage.setItem('user', { name: 'Pietro' });
-    axios.post = jest.fn(() => new Promise((resolve, reject) => {
-      resolve();
-    }));
   });
 
   describe('WHEN I logout', () => {
-    beforeAll(async () => {
-      await authentication.logout();
+    beforeAll(() => {
+      axiosMock.post.mockResolvedValueOnce();
+      authentication.logout();
     });
 
     test('THEN The logout url is called', () => {
-      expect(axios.post.mock.calls.length).to.equal(1);
-      expect(axios.post.mock.calls[0][0]).to.equal('/api/auth/logout');
+      expect(axiosMock.post.mock.calls.length).to.equal(1);
+      expect(axiosMock.post.mock.calls[0][0]).to.equal('/api/auth/logout');
     });
 
     test('THEN The user is not authenticated', () => {
