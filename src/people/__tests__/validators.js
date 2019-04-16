156c1633fc0c9bf8fd4447a19e19d994fc9d690d
Pietro Carta <pietro.carta88@gmail.com>
2019-04-16 09:16:34 +0100

    person validator tests

---
+++ src/people/__tests__/validators.js
@@ -0,0 +1,38 @@
+import { personValidator } from '../validators';
+
+const expectedPersonValidator = {
+	firstName: {
+		presence: {
+			allowEmpty: false,
+			message: ';Inserisci un nome'
+		},
+		length: { maximum: 255, tooLong: ';Il nome e troppo lungo' },
+  },
+  lastName: {
+		presence: {
+			allowEmpty: false,
+			message: ';Inserisci un cognome'
+		},
+		length: { maximum: 255, tooLong: ';Il cognome e troppo lungo' },
+  },
+  phone: {
+    numericality: {
+			onlyInteger: true,
+			message: ';Il numero di telefono non e valido'
+    },
+		length: { maximum: 255, tooLong: ';Il numero di telefono e troppo lungo' },    
+  },
+  address: {
+		length: { maximum: 255, tooLong: ';L\'indirizzo e troppo lungo' },
+  },
+  email: {
+    email: {
+      message: ';L\'email non e valida'
+    }
+  }
+};
+
+
+test('person validator is correct', () => {
+  expect(personValidator).toEqual(expectedPersonValidator);
+});
